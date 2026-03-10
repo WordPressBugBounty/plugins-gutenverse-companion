cc1369d52a5bd0ef54f63768a33f3fb378efd4a4
Sean Murphy <sean@iamseanmurphy.com>
2026-03-10 07:28:26 +0000

    2.1.6

--- gutenverse-companion/lib/dependencies/lite-plus-dashboard.asset.php
+++ gutenverse-companion/lib/dependencies/lite-plus-dashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'c627059b6084915b1d88');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '5c7805957307299015ab');
