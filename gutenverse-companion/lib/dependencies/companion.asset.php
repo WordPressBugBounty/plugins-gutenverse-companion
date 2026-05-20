52ca1c7c03f23133054cd2851a26c7875516f796
Sean Murphy <sean@iamseanmurphy.com>
2026-05-20 05:34:34 +0000

    2.4.1

--- gutenverse-companion/lib/dependencies/companion.asset.php
+++ gutenverse-companion/lib/dependencies/companion.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => '9ab97f2ce9af66fd5488');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => '4d42e756991bd20fbf04');
